2b248f0552194519557e7932f660267c988da76b
saracj <sara.jonvik@gmail.com>
2018-01-25 12:36:40 +0100

    updated the first plotting function

--- Exploratory Data Analysis/plot1.R
+++ Exploratory Data Analysis/plot1.R
@@ -1,9 +1,17 @@
 #setwd("datasciencecoursera/Exploratory Data Analysis/")
-houshold_power_consumption <- read.csv(file="household_power_consumption.txt", header=TRUE, sep=";", na.strings="?")
-
 
 ### Plotting a histogram of the frequency of global active power in the households
+### only using the two given days 1/2/2007 and 2/2/2007, and saving the resulting 
+### histogram to plot1.png
 
-global_active <- houshold_power_consumption$Global_active_power
+household_power_consumption <- read.table(file="household_power_consumption.txt", header=TRUE, sep=";", na.strings="?")
 
-hist(global_active, col="red", xlab="")
+split_data <- split(household_power_consumption, household_power_consumption$Date)
+rel_data <- rbind(split_data$`1/2/2007`, split_data$`2/2/2007`)
+
+
+global_active <- rel_data$Global_active_power
+
+png("plot1.png")
+hist(global_active, col="red", xlab="Global Active Power (kilowatts)", main="Global Active Power")
+dev.off()
