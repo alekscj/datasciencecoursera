b51e63e1c68a74cb899833d390cdd499822dc741
saracj <sara.jonvik@gmail.com>
2018-01-26 13:31:02 +0100

    Completed the last plot, plot4.R. Committing everything for submission of course project 1

--- Exploratory Data Analysis/plot4.R
+++ Exploratory Data Analysis/plot4.R
@@ -16,3 +16,19 @@ rel_data <- rbind(rel_data$`1/2/2007`, rel_data$`2/2/2007`)
 
 #converted_date_time <- vector(mode = "list", length = length(rel_data$Date))
 rel_data$DateTime <- strptime(paste(rel_data$Date, rel_data$Time), format = "%d/%m/%Y %H:%M:%S")
+
+png("plot4.png")
+par(mfrow=c(2,2))
+
+plot(rel_data$DateTime, rel_data$Global_active_power, type="l", ylab="Global Active Power (kilowatts)", xlab=" " )
+
+plot(rel_data$DateTime, rel_data$Voltage, type="l", ylab="Voltage", xlab="datetime")
+
+plot(rel_data$DateTime, rel_data$Sub_metering_1, type="l", col="black", ylab="Energy sub metering", xlab=" " )
+lines(rel_data$DateTime, rel_data$Sub_metering_2, col="red")
+lines(rel_data$DateTime, rel_data$Sub_metering_3, col="blue")
+legend("topright", c(legend="Sub metering 1", "Sub metering 2", "Sub metering 3"), lty=1, col=c("black", "red", "blue"))
+
+plot(rel_data$DateTime, rel_data$Global_reactive_power, type="l", ylab="Global Active Power (kilowatts)", xlab="datetime" )
+
+dev.off()
