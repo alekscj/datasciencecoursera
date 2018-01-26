d62ed68677c746da5149bcf9890e5ccc2b7b5885
saracj <sara.jonvik@gmail.com>
2018-01-26 12:51:55 +0100

    Fixed the date-time conversion and completed plot number 2.

--- Exploratory Data Analysis/plot2.R
+++ Exploratory Data Analysis/plot2.R
@@ -1,19 +1,25 @@
 #setwd("datasciencecoursera/Exploratory Data Analysis/")
-
+library(stringi)
 ### 
 ### only using the two given days 1/2/2007 and 2/2/2007, and saving the resulting 
 ### histogram to plot1.png
 
+#rel_data <- read.table(file="household_power_consumption.txt", header=TRUE, sep=";", na.strings="?")
 household_power_consumption <- read.table(file="household_power_consumption.txt", header=TRUE, sep=";", na.strings="?")
 
-split_data <- split(household_power_consumption, household_power_consumption$Date)
-rel_data <- rbind(split_data$`1/2/2007`, split_data$`2/2/2007`)
+rel_data <- split(household_power_consumption, household_power_consumption$Date)
+rel_data <- rbind(rel_data$`1/2/2007`, rel_data$`2/2/2007`)
+#split_data <- split(household_power_consumption, household_power_consumption$Date)
 
 
 ### Converting from given label to date format in R and splitting the data in weekdays: 
 
-#date_converted <- weekdays(as.Date(rel_data$Date))
-time_converted <- strptime( as.character(rel_data$Time), "%H:%M:%S")
-time_converted <- format(time_converted, "%H:%M:%S")
-#rel_data <- cbind(date_converted, time_converted, rel_data[, 3:9])
-#weekday_data <- split(rel_data,rel_data$date_converted)
+
+#converted_date_time <- vector(mode = "list", length = length(rel_data$Date))
+rel_data$DateTime <- strptime(paste(rel_data$Date, rel_data$Time), format = "%d/%m/%Y %H:%M:%S")
+
+
+
+#png("plot2.png")
+plot(rel_data$DateTime, rel_data$Global_active_power, type="l", ylab="Global Active Power (kilowatts)", xlab=" " )
+#dev.off()
